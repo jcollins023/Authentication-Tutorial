89a563c2ae1147c89bb1c1079b6f19a36fb1b284
jcollins023 <johncollins023@gmail.com>
2025-01-17 11:56:41 -0500

    Initial Commit

---
+++ Authentication Tutorial/Authentication_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Authentication_TutorialApp.swift
+//  Authentication Tutorial
+//
+//  Created by John Collins on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct Authentication_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Authentication TutorialTests/Authentication_TutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  Authentication_TutorialTests.swift
+//  Authentication TutorialTests
+//
+//  Created by John Collins on 1/17/25.
+//
+
+import Testing
+@testable import Authentication_Tutorial
+
+struct Authentication_TutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
